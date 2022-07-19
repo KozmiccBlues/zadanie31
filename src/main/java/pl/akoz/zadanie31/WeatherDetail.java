600e8cfa5f0be9a75ce2d0c51d2c83b20fada987
Ola <kozlowska.aleksandra92@gmail.com>
2022-07-19 21:04:00 +0200

    solution 1

---
+++ src/main/java/pl/akoz/zadanie31/WeatherDetail.java
@@ -0,0 +1,17 @@
+package pl.akoz.zadanie31;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Data
+public class WeatherDetail {
+
+    private String city;
+    private double temperature;
+
+
+
+}
+
